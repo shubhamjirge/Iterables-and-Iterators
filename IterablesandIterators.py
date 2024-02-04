d345a2b92471b835ae99f2eb792adf8c92ce4ebd
shubhamjirge <141141304+shubhamjirge@users.noreply.github.com>
2024-02-04 23:09:24 +0530

    Add files via upload

---
+++ IterablesandIterators.py
@@ -0,0 +1,13 @@
+"""
+Title     : Iterables and Iterators
+Subdomain : Itertools
+Domain    : Python
+"""
+from itertools import combinations
+
+n = int(input())
+ar = input().split()
+k = int(input())
+comb_list = list(combinations(ar, k))
+a_list = [e for e in comb_list if "a" in e]
+print(len(a_list) / len(comb_list))
